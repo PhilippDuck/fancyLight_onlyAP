526d1e08cd373b559ea90c663a32c2e0a0e63e23
Philipp Duck <phil.mar.du@gmail.com>
2021-12-23 10:57:21 +0100

    Added FancyMode switchbutton and connection to board

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,80 @@
+// Libaries
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <LittleFS.h>
 
+// Define name and password from Acces Point
 #ifndef APSSID
-#define APSSID "Test"
+#define APSSID "myFancyLight"
 #define APPSK  "passwort"
 #endif
 
-/* Set these to your desired credentials. */
+// Set these to your desired credentials.
 const char *ssid = APSSID;
 const char *password = APPSK;
 
+// Create Server
 ESP8266WebServer server(80);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
-   connected to this access point to see it.
-*/
+// Variable declaration
+String fancyMode; 
+int red = 0;
+int grn = 0;
+int blu = 0;
 
+// Function that gets colorstatus from server
+void getColor() {
+  String r = server.arg("r");
+  String g = server.arg("g");
+  String b = server.arg("b");
+  fancyMode = server.arg("fancyMode");
+  red = r.toInt();
+  grn = g.toInt();
+  blu = b.toInt();
+  Serial.println("");
+  Serial.println("New Color:");
+  Serial.println("RGB: " + r + " " + g + " " + b + ", FancyMode: " + fancyMode);
+  /*
+  if (fancyMode == "false"){
+    for (int i = 0; i < LEDS; i++){
+      strip.setPixelColor(i,red, grn, blu);
+      strip.show();
+    }
+  }*/
+
+  server.send(200, "text/plain", "OK");
+}
+
+// Initial Routine
 void setup() {
+
+  // Give the Chip some time
   delay(1000);
+
+  // Open Serial connection for debugging
   Serial.begin(115200);
   Serial.println();
-  Serial.print("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid, password);
 
+  // Open AP
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
 
+  // Make the Flashstorage accessible
   LittleFS.begin();
+
+  // Start Webserver and listen to requests
   server.serveStatic("/", LittleFS, "/index.html");
+  server.serveStatic("/iromin.js", LittleFS, "/iromin.js");
+  server.on("/sendColor", getColor);
   server.begin();
   Serial.println("HTTP server started");
-  server.serveStatic("/iromin.js", LittleFS, "/iromin.js");
 }
 
+// Programm loop
 void loop() {
   server.handleClient();
 }
